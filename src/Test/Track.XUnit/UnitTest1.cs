5956c5e06dc04c1e3586faa1bc3567753667b0b6
Arthur Yole <artyole@hotmail.com>
2018-09-12 14:31:32 -0300

    criação do projeto de UnitTest

--- src/Test/Track.XUnit/UnitTest1.cs
+++ src/Test/Track.XUnit/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using Track.Domain.ClearSale.Models;
 
 namespace Track.XUnit
 {
+     
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void EnviarRequestVazio()
         {
+            SendDataLoginResponse sendDataLoginResponse = new SendDataLoginResponse();
 
+            int expectedStatusCode = 200;
+
+            
         }
     }
 }
