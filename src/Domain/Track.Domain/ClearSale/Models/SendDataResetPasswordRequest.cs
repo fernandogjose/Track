58f336fdf50cc2c0d4b3201f3ca82f2f2f530dc1
Vagner da Veiga Moreira <vagner.moreira@viavarejo.com.br>
2018-09-18 16:33:11 -0300

    Inclusao campo email

--- src/Domain/Track.Domain/ClearSale/Models/SendDataResetPasswordRequest.cs
+++ src/Domain/Track.Domain/ClearSale/Models/SendDataResetPasswordRequest.cs
@@ -5,6 +5,9 @@ namespace Track.Domain.ClearSale.Models {
 
     public class SendDataResetPasswordRequest {
 
+        [JsonProperty (PropertyName = "email")]
+        public string Email { get; set; }
+
         [JsonProperty (PropertyName = "code")]
         public string Code { get; set; }
 
