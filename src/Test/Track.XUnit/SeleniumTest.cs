5e2cf7769b92cfe915d91910408423629dbf6fe8
Arthur Yole <artyole@hotmail.com>
2018-11-26 12:56:20 -0200

    correção de aspas faltantes

--- src/Test/Track.XUnit/SeleniumTest.cs
+++ src/Test/Track.XUnit/SeleniumTest.cs
@@ -11,12 +11,11 @@ namespace Track.XUnit {
             [Fact]
             public void ChromeDriverTest () {
                 using (var driver = new ChromeDriver (Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location))) {
-                    driver.Navigate ().GoToUrl (@"http://localhost:4200/#/);
+                    driver.Navigate ().GoToUrl (@"http://localhost:4200/#/");
                     var link = driver.FindElement (By.PartialLinkText ("TFS Test API"));
                     var jsToBeExecuted = $"window.scroll(0, {link.Location.Y});";
                     ((IJavaScriptExecutor) driver).ExecuteScript (jsToBeExecuted);
                     var wait = new WebDriverWait (driver, TimeSpan.FromMinutes (1));
-                    
                 }
             }
         }
